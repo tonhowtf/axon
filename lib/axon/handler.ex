1f5c97deee4afd620609e8f1ef4b1215b03e1e62
Tonho <antonioreverso.com@gmail.com>
2025-07-27 22:39:44 -0300

    Implement request handling with parsing, routing, and response formatting

--- lib/axon/handler.ex
+++ lib/axon/handler.ex
@@ -1,5 +1,40 @@
 defmodule Axon.Handler do
   def handle(request) do
+    # conv = parse(request)
+    # conv = route(conv)
+    # format_response(conv)
+
+    request
+    |> parse
+    |> route
+    |> format_response
+  end
+
+  def parse(request) do
+    # TODO: Parse the request string into a map:
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first()
+      |> String.split(" ")
+
+    %{method: method, path: path, resp_body: ""}
+  end
+
+  def route(conv) do
+    # TODO: Create a new map that also has the response body:
+    %{conv | resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def format_response(conv) do
+    # TODO: Use values in the map to create an HTTP response string:
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: #{String.length(conv.resp_body)}
+
+    #{conv.resp_body}
+    """
   end
 end
 
@@ -8,15 +43,6 @@ GET /wildthings HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
-"""
-
-expected_response = """
-HTTP/1.1 200 OK
-Content-Type: text/html
-Content-Length: 20
-
-
-Bears, Lions, Tigers
 """
 
 response = Axon.Handler.handle(request)
